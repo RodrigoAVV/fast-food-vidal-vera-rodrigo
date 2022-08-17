13b018dec6261588a9cf388e5f749d330316a666
Rodrigo Antonio <rodrigo.vidal@virginiogomez.cl>
2022-08-17 12:24:41 -0400

    Se agrega cantidad de producto en item del carrito

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom'
 
 const Products = ({producto}) => {
     return(
-        <div className="col-sm-3" style={{marginTop:10}}>
-           <img src={"/src/components/products/img/"+producto.imagen} className="card-img-top" alt="..." style={{height:250,width:300}}/>
-            <div className="card-body">
-            <h5 className="card-title">{producto.nombre}</h5>
-                <p className="card-text">{producto.descripcion}</p>
+       
+            <div className="col-sm-3" style={{marginTop:10}}>
+            <img src={"/src/components/products/img/"+producto.imagen} className="card-img-top" alt="..." style={{height:250,width:300}}/>
+                <div className="card-body">
+                <h5 className="card-title mb-0">{producto.nombre}</h5>
+                <p className="card-text mb-0">{producto.descripcion}</p>
+                <p className="card-text mb-0">{producto.precio}</p>
+                </div>
+                <Link to={"/item/"+producto.codigo} className="btn btn-info" style={{width:300}} id={producto.codigo}>Detalle</Link>
             </div>
-            <Link to={"/item/"+producto.codigo} className="btn btn-info" style={{width:300}} id={producto.codigo}>Detalle</Link>
-        </div>
+        
     )
 }
 
