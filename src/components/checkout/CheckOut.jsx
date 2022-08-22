bddd6e0e835fd9fcda51139e765c786f83c4b019
Rodrigo Antonio <rodrigo.vidal@virginiogomez.cl>
2022-08-22 13:03:52 -0400

    Se crea data para registro en base de datos

--- src/components/checkout/CheckOut.jsx
+++ src/components/checkout/CheckOut.jsx
@@ -1,20 +1,31 @@
 import React,{useState,useEffect} from 'react'
 import { useCartContext } from '../cartcontext/CartContext'
 import { Link } from 'react-router-dom'
+import {addDoc,collection,getFirestore} from 'firebase/firestore'
 
 
 const CheckOut = () => {
 
     const {productList,totalPrecio} = useCartContext()
     
-
+    const [total,setTotal] = useState(0)
+    useEffect(()=>{
+        setTotal(totalPrecio())
+    },[total])
+    
     const [nombre,setNombre] = useState('')
     const [telefono,setTelefono] = useState('')
     const [correo,setCorreo] = useState('')
 
     const terminarCompra = () => {
-        const order = {buyer: {nombre,telefono,correo},items:[productList],totalPrecio}
+        const order = {buyer: {nombre,telefono,correo},items:[productList],total:total}
+        const db = getFirestore()
+        const refCollection = collection(db,'orders')
         console.log(order)
+        addDoc(refCollection,order).then((res)=>{
+           console.log(res)
+        })
+        
     }
 
     return(
@@ -27,7 +38,7 @@ const CheckOut = () => {
             <span className="input-group-text">@</span>
             <input type={'email'} className="form-control" placeholder="Correo" aria-label="Correo" value={correo}
                 onChange={(e)=>setCorreo(e.target.value)}/>
-                <Link to={'/checkout'} type="button" className="btn btn-success" onClick={() => terminarCompra()}>Success</Link>
+                <button type="button" className="btn btn-success" onClick={() => terminarCompra()}>Success</button>
         </div>                      
     )
 }
